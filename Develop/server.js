fb14b5ec96d94c34896dec4f7f6187174eff1269
CharissaHollister <charissa.hollister@diasorin.com>
2022-06-29 18:07:53 -0500

    add new note to post

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 
 // generating unique ids
 const generateUniqueId = require("generate-unique-id");
+const { get } = require("http");
 
 // Initialize and parse express app
 const PORT = process.env.PORT || 3001;
@@ -13,12 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/notes", (req, res) =>
+/////////////////pull in routes
+// require("./public/routes/routes.js");
+
+//populate index.html file for the website on landing
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-/////////////////pull in routes
-// require("./public/routes/routes.js");
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//get notes from db and parse them
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
@@ -33,6 +41,29 @@ app.get("/api/notes", (req, res) => {
     }
   });
 });
+
+//look for new notes and add to db
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      ////need to add logic to not fail if notes is empty?
+      let notes = [];
+      notes.push(JSON.parse(data));
+      console.log("notes observed");
+      notes.push(req.body);
+      fs.writeFile("./db/db.json", notes, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("written successfully");
+      });
+    }
+  });
+});
+
 /////////////end of routes
 
 //run app listener
